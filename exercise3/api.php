254dc60db919ced13e41571f86203bb04e5dc916
Angel David Velasco <82744167+AngelVelasco1@users.noreply.github.com>
2023-05-28 11:36:31 -0500

    feat:

--- exercise3/api.php
+++ exercise3/api.php
@@ -1,10 +1,3 @@
 <?php
-    $number = $_POST['number'];
-
-    if($number % 2  == 0 && $number > 10) {
-        echo "The number is even and greater than 10";
-    }
-    else if($number) {
-
-    }
+ 
 ?>
